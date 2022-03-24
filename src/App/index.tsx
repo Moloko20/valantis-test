8bf9687c8ddcc549b611a87e8fe9fcf7eb47214b
Moloko20 <alexvfrontend@gmail.com>
2022-03-24 17:24:48 +0300

    feat(app): add base structure

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { CurrencyList } from 'Components/CurrencyList'
 
 function App() {
-    require('./index.scss')
+    require('./index.sass')
 
-    return <div></div>
+    return (
+        <div className="wrapper">
+            <h1>Курсы валют</h1>
+            <CurrencyList />
+        </div>
+    )
 }
 
 ReactDOM.render(
