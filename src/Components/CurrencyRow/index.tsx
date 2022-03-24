b5dfc9aff23d1f960108b4f2e938293cc3e1c80b
Moloko20 <alexvfrontend@gmail.com>
2022-03-24 18:48:52 +0300

    feat(currency row): change different cell calculation

--- src/Components/CurrencyRow/index.tsx
+++ src/Components/CurrencyRow/index.tsx
@@ -17,11 +17,29 @@ export function CurrencyRow({ currency }: CurrencyRowPropsType) {
 
     console.log(currency)
 
+    let different: number | string = +(
+        (currency.Value / 100) *
+        (currency.Value - currency.Previous)
+    ).toFixed(1)
+
+    let differentClassName: string = ''
+
+    if (different > 0) {
+        differentClassName = 'different--up'
+    } else if (different < 0) {
+        differentClassName = 'different--down'
+        different = Math.abs(different)
+    }
+
+    if (String(different).length < 2) {
+        different = different + '.0'
+    }
+
     return (
         <tr>
             <td>{currency.CharCode}</td>
             <td>{currency.Value}</td>
-            <td>{(currency.Value - currency.Previous).toFixed(3)}</td>
+            <td className={differentClassName}>{different}</td>
         </tr>
     )
 }
