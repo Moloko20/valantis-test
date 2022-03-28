037698ff53187bfe140612f4aff93b99099f9842
Moloko20 <alexvfrontend@gmail.com>
2022-03-28 12:46:38 +0300

    perf(app): change structure

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Context } from 'Contexts/context'
+
 import { CurrencyList } from 'Components/CurrencyList'
+import { CurrencyPopup } from 'Components/CurrencyPopup'
 
 function App() {
+    const [currentCurrencyName, setCurrentCurrencyName] = React.useState('')
+
     require('./index.sass')
 
     return (
-        <div className="wrapper">
-            <h1>Курсы валют</h1>
-            <CurrencyList />
-        </div>
+        <Context.Provider
+            value={{
+                setCurrentName: setCurrentCurrencyName,
+            }}
+        >
+            <div className="wrapper">
+                <div className="wrapper-container">
+                    <CurrencyList />
+                    <CurrencyPopup currentName={currentCurrencyName} />
+                </div>
+
+                <a href="https://www.cbr-xml-daily.ru/">Виджет курсов валют</a>
+            </div>
+        </Context.Provider>
     )
 }
 
