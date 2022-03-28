a51174f32a165d338c03c57f17174370d352eddd
Moloko20 <alexvfrontend@gmail.com>
2022-03-29 00:49:48 +0300

    feat(services): add get currency history function

--- src/Services/getCurrency.ts
+++ src/Services/getCurrency.ts
@@ -1,4 +1,45 @@
-export async function getCurrency(url?: string) {
-    if (!url) url = 'https://www.cbr-xml-daily.ru/daily_json.js'
-    return await fetch(url).then(response => response.json())
+export interface ValuteItem {
+    CharCode: string
+    ID: string
+    Name: string
+    Nominal: number
+    NumCode: string
+    Previous: number
+    Value: number
+}
+
+export interface CurrencyData {
+    Date: string
+    PreviousDate: string
+    PreviousURL: string
+    Timestamp: string
+    Valute: {
+        [key: string]: ValuteItem
+    }
+}
+
+export function getCurrency(
+    url: string = 'https://www.cbr-xml-daily.ru/daily_json.js',
+): Promise<CurrencyData> {
+    return fetch(url).then(response => response.json())
+}
+
+export async function getCurrencyHistory(
+    currencyURL: string,
+    count: number = 10,
+): Promise<Array<CurrencyData>> {
+    const result: Array<CurrencyData> = []
+
+    let iterationsCount: number = count
+    let iterationURL: string = currencyURL
+
+    while (iterationsCount > 0) {
+        const data = await getCurrency(iterationURL)
+        result.push(data)
+
+        iterationsCount--
+        iterationURL = data.PreviousURL
+    }
+
+    return result
 }
