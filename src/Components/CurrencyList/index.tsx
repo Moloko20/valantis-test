543213d70d809d222e8eee62e5ca2b72fab0489e
Moloko20 <alexvfrontend@gmail.com>
2022-03-29 00:46:55 +0300

    perf(currency list): change structure

--- src/Components/CurrencyList/index.tsx
+++ src/Components/CurrencyList/index.tsx
@@ -1,21 +1,90 @@
 import React from 'react'
 
-import { CurrencyTHead } from 'Components/CurrencyTHead'
-import { CurrencyTBody } from 'Components/CurrencyTBody'
+import { Context } from 'Contexts/context'
 
-import { getCurrency } from 'Services/getCurrency'
+import { getCurrency, ValuteItem } from 'Services/getCurrency'
 
-function CurrencyListComponent() {
-    const headTitles: string[] = ['Наименование валюты', 'Значение, ₽', 'Разница, %']
+import { Loader } from 'Components/UI/Loader'
+import { Tooltip } from 'Components/UI/Tooltip'
 
-    const [valutes, setValutes] = React.useState([])
+const HEAD_TITLES: string[] = ['Наименование валюты', 'Значение, ₽', 'Разница, %']
+
+function useDebounce<T>(value: T, delay?: number): T {
+    const [debouncedValue, setDebouncedValue] = React.useState<T>(value)
 
     React.useEffect(() => {
-        getCurrency().then(data => {
-            const arr = Object.entries(data.Valute).map(value => value[1])
+        const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
 
-            setValutes(arr)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [value, delay])
+
+    return debouncedValue
+}
+
+function CurrencyListComponent() {
+    const [valutes, valutesSet] = React.useState<Array<ValuteItem>>([])
+    const [loading, loadingSet] = React.useState<boolean>(false)
+
+    const [currentCurrencyName, currentCurrencyNameSet] = React.useState('')
+    const debouncedName = useDebounce<string>(currentCurrencyName, 400)
+
+    const [tooltipCoords, tooltipCoordsSet] = React.useState([0, 0])
+    const debouncedCords = useDebounce<Array<number>>(tooltipCoords, 300)
+
+    const [differents, differentsSet] = React.useState<Array<string>>([])
+
+    const context = React.useContext(Context)
+
+    React.useEffect(() => {
+        loadingSet(true)
+
+        getCurrency()
+            .then(data => {
+                valutesSet(Object.values(data.Valute))
+            })
+            .finally(() => {
+                loadingSet(false)
+            })
+    }, [])
+
+    React.useEffect(() => {
+        differentsSet([])
+
+        valutes.forEach(obj => {
+            if (!obj) {
+                console.log('yes')
+                differentsSet(prevState => [...prevState, '0'])
+            } else {
+                let different = (
+                    Math.round(+(100 - (obj.Previous * 100) / obj.Value).toFixed(3) * 10) / 10
+                ).toString()
+
+                if (different.length < 3) {
+                    different = different + '.0'
+                }
+
+                differentsSet(prevState => [...prevState, different])
+            }
         })
+    }, [valutes])
+
+    const rowClickHandler = React.useCallback((currencyName: string) => {
+        context.setCurrentName(currencyName)
+    }, [])
+
+    const rowHoverHandler = React.useCallback((currencyName: string) => {
+        currentCurrencyNameSet(currencyName)
+    }, [])
+
+    const rowUnHoverHandler = () => {
+        currentCurrencyNameSet('')
+        tooltipCoordsSet([0, 0])
+    }
+
+    const rowMoveHandler = React.useCallback((event: React.MouseEvent) => {
+        tooltipCoordsSet([event.pageX, event.pageY])
     }, [])
 
     require('./index.sass')
@@ -25,9 +94,54 @@ function CurrencyListComponent() {
             <div className="section-header">
                 <h1>Курсы валют</h1>
             </div>
+
             <table className="currency">
-                <CurrencyTHead titles={headTitles} />
-                <CurrencyTBody valutes={valutes} currencysCount={0} />
+                <thead className="currency-head">
+                    <tr>
+                        {HEAD_TITLES.map(title => (
+                            <th key={title}>{title}</th>
+                        ))}
+                    </tr>
+                </thead>
+
+                <tbody className="currency-body">
+                    {!loading ? (
+                        valutes.map((currencyItem, index) => (
+                            <tr
+                                onMouseEnter={() => rowHoverHandler(currencyItem.Name)}
+                                onMouseLeave={rowUnHoverHandler}
+                                onMouseMove={rowMoveHandler}
+                                onClick={() => rowClickHandler(currencyItem.Name)}
+                                key={currencyItem.ID}
+                            >
+                                <td>{currencyItem.CharCode}</td>
+                                <td>{currencyItem.Value}</td>
+                                <td
+                                    className={
+                                        +differents[index] >= 0
+                                            ? 'different--up'
+                                            : 'different--down'
+                                    }
+                                >
+                                    {differents[index]}
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={3} className="loading">
+                                <Loader />
+                            </td>
+                        </tr>
+                    )}
+                    <tr>
+                        <td className="tooltip_wrapper">
+                            {debouncedName && (
+                                <Tooltip value={debouncedName} coords={debouncedCords} />
+                            )}
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </section>
     )
